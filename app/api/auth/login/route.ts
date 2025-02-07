ace9541dd8222452a31d70d7e898289df1684693
ZiaAlam7 <ziaalam1446@gmail.com>
2025-02-07 19:34:20 +0500

    Sign In Successfully

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,48 @@
+import { connectToDatabase } from "@/lib/mongodb";
+import UserAuth from "@/models/User";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: NextRequest){
+
+    try {
+        const {email, password} = await request.json()
+
+        if(!email || !password){
+            return NextResponse.json(
+                {error: "Email or Password is missing" },
+                {status: 400}
+            )
+        }
+
+        await connectToDatabase()
+
+        const registeredUser = await UserAuth.findOne({email})
+
+        if(!registeredUser){
+            return NextResponse.json(
+                {error: "User does not exist"},
+                {status: 400}
+            )
+        }
+
+        const passValid = await bcrypt.compare(password , registeredUser.password)
+        if(!passValid){
+            return NextResponse.json(
+                {error: "Incorrect Credentials"},
+                {status: 400}
+            )
+        }
+
+        return NextResponse.json(
+            { message: "Sign In Successfully", success:true},
+            { status: 201 }
+        );
+
+    } catch (error) {
+         return NextResponse.json(
+            { error: "Failed to authenticate the user" },
+            { status: 500 }
+        );
+    }
+}
